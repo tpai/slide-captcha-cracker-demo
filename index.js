0bc8a88c4640932236caf488a71e35ab3dca5390
Tony Pai <tony77794@gmail.com>
2020-08-01 18:29:43 +0800

    Refactor and add comments

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const { get, send } = require("./api");
 
 const start = async function() {
   const json = await get();
-
   const source = JSON.parse(json.data);
-  const bigImage = source.bigImage.replace(/^data:image\/\w+;base64,/, "");
-
-  const index = await getFirstGreenPixel(bigImage, source.yHeight);
+  const index = await getFirstGreenPixel(source.bigImage, source.yHeight);
   console.log(`answer=${index}`);
   const result = await send(index, json.JSESSIONID);
   console.log(result.info);
--- parseImage.js
+++ parseImage.js
@@ -2,17 +2,15 @@ const getPixels = require("get-pixels");
 
 function getFirstGreenPixel(bigImage, yHeight) {
   return new Promise((resolve, reject) => {
-    getPixels(Buffer.from(bigImage, "base64"), "image/png", (err, pixels) => {
+    // remove meta information for creating buffer
+    const image = bigImage.replace(/^data:image\/\w+;base64,/, "");
+    getPixels(Buffer.from(image, "base64"), "image/png", (err, pixels) => {
       if (err) {
         return reject(err);
       }
       for (var i = 0; i < pixels.shape[0]; i++) {
-        // console.log(
-        //   pixels.get(i, yHeight, 0),
-        //   pixels.get(i, yHeight, 1),
-        //   pixels.get(i, yHeight, 2),
-        //   pixels.get(i, yHeight, 3)
-        // );
+        // the target is a green pixel, the distance
+        // from the pixel to the left border is answer
         if (
           pixels.get(i, yHeight, 0) === 0 &&
           pixels.get(i, yHeight, 1) === 255 &&
@@ -24,19 +22,6 @@ function getFirstGreenPixel(bigImage, yHeight) {
         }
       }
       resolve(0);
-      // for (var i = 0; i < img.width(); i++) {
-      //   const pixel = img.getPixel(i, yHeight);
-      //   if (
-      //     pixel.r === 0 &&
-      //     pixel.g === 255 &&
-      //     pixel.b === 0 &&
-      //     pixel.a === 1
-      //   ) {
-      //     // console.log(i, img.getPixel(i, yHeight));
-      //     resolve(i - 1);
-      //     break;
-      //   }
-      // }
     });
   });
 }
