eec2c50ffdc077039fdd1124ce97193201ffd80f
Tony Pai <tony77794@gmail.com>
2020-08-01 18:40:19 +0800

    Update comments

--- parseImage.js
+++ parseImage.js
@@ -8,9 +8,14 @@ function getFirstGreenPixel(bigImage, yHeight) {
       if (err) {
         return reject(err);
       }
+      // get-pixels returns ndarray which stores massive color information
+      // https://www.npmjs.com/package/ndarray
       for (var i = 0; i < pixels.shape[0]; i++) {
-        // the target is a green pixel, the distance
-        // from the pixel to the left border is answer
+        // the target is a green pixel
+        // the distance from the pixel to left border is the answer
+
+        // since we knew the height(yHeight)
+        // we can easily find the pixel without scan from the top
         if (
           pixels.get(i, yHeight, 0) === 0 &&
           pixels.get(i, yHeight, 1) === 255 &&
